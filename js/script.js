47219de10c6e309b9f566612b51389129a7a20ff
ccapora2 <ccapora2@jhu.edu>
2020-02-13 11:48:04 -0500

    Critical feature: spinning copyright symbol

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('Hello, World!');
+
+jQuery(function($){
+	
+	$('.copyright span').on('click', function(e){
+			console.log('test');
+			console.log($(e.target));
+			
+			$target = $(e.target);
+			$target.toggleClass('spinny');
+		}
+	);
+	
+});
